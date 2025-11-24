edbc7dc9ea478116ef422010423e3a25f94acf15
badigadiii <bigtinyturtle@gmail.com>
2025-11-25 01:16:52 +0400

    update models

--- src/models/users.py
+++ src/models/users.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import String, Boolean, UUID
+from sqlalchemy import String, Boolean, UUID, text
 from sqlalchemy.orm import mapped_column, Mapped
 
 from .base import Base
@@ -10,7 +10,12 @@ class Users(Base):
     __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        server_default=text("gen_random_uuid()"),
     )
     username: Mapped[str] = mapped_column(String(50), nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, default=True, server_default="true"
+    )
