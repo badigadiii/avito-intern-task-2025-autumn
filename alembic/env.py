d58069cb8061a6c0921b4fd9d897c8d0e2ea75ce
badigadiii <bigtinyturtle@gmail.com>
2025-11-25 01:00:21 +0400

    update alembic config and update models

---
+++ alembic/env.py
@@ -0,0 +1,57 @@
+import asyncio
+from logging.config import fileConfig
+
+import src.models # after adding this finally
+from src.models import Base
+from src.config import settings
+
+from alembic import context
+from sqlalchemy.ext.asyncio import create_async_engine
+
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+DB_URL = settings.db_url
+
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+        compare_server_default=True,
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations():
+    engine = create_async_engine(DB_URL, pool_pre_ping=True)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(do_run_migrations)
+
+    await engine.dispose()
+
+
+def run_migrations_offline():
+    url = DB_URL
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    asyncio.run(run_async_migrations())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
